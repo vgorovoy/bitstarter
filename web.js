5cf363f8789dcb245a4654c13b56628ffd96651a
Vladimir.Gorovoy <Vladimir.Gorovoy@gmail.com>
2013-07-11 20:59:46 +0000

    reading from file - fix

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buffer = fs.readFileSyn('index.html');
+  var buffer = fs.readFileSync('index.html');
   response.send(buffer.toString());
 });
 
